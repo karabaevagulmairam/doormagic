645a1107a29243a26d67c566ffcfd951de073397
JiYeonaa <yeonniseon@gmail.com>
2023-09-03 00:02:01 +0600

    love

--- src/components/ProductInfo/ProductInfo.jsx
+++ src/components/ProductInfo/ProductInfo.jsx
@@ -1,7 +1,17 @@
-import React from 'react';
-import {Link} from 'react-router-dom'
+import React, {useContext} from 'react';
+import {Link, useNavigate} from 'react-router-dom';
+import {AiOutlineHeart, AiFillHeart} from "react-icons/ai";
+import {LiaOpencart} from   "react-icons/lia"
+import {CustomContext} from "../../config/context/context";
 
 const ProductInfo = ({product}) => {
+
+    const {favorites, favoritesHandler} = useContext(CustomContext);
+
+    const {addCarts, user} = useContext(CustomContext);
+
+    const navigate = useNavigate();
+
     return (
         <div className="product__info">
             <div className="container">
@@ -10,28 +20,44 @@ const ProductInfo = ({product}) => {
                         {product.title}
                     </h2>
                     <Link to={'/'} className="product__info-author">
-                        {product.author}
-                        (Автор)
+                        {product.author} (Автор)
                     </Link>
 
-                    <ul className="product__info-ul">
-                        <li className="product__info-li">Издательство: <span className="product__info-span">{product.public}</span></li>
-                        <li className="product__info-li">Жанр: <span className="product__info-span">{product.category}</span></li>
-                        <li className="product__info-li">Переплет: <span className="product__info-span">{product.book}</span></li>
-                        <li className="product__info-li">Возрастное ограничение: <span className="product__info-span">{product.limit}</span></li>
-                    </ul>
+                    <div className="product__info-smell">
+                        <h3 className="product__info-char">Характеристики</h3>
 
-                    <div className="product__info-buy">
-                        <form action="" className="product__info-form">
-                            <span className="product__info-quantity">Quantity</span>
-                            <input type="number" className="product__info-num" min={1} max={9}/>
-                            <span className="product__info-sell">{product.price}</span>
-                        </form>
+                        <ul className="product__info-ul">
+                            <li className="product__info-li">Издательство:................................. <span className="product__info-span">{product.public}</span></li>
+                            <li className="product__info-li">Жанр:.................................................... <span className="product__info-span">{product.category}</span></li>
+                            <li className="product__info-li">Переплет:.......................................... <span className="product__info-span">{product.book}</span></li>
+                            <li className="product__info-li">Возрастное ограничение:...... <span className="product__info-span">{product.limit}</span></li>
+                        </ul>
                     </div>
 
                     <div className="product__info-btns">
-                        <button className="product__info-btn">Добавить в Корзину</button>
-                        <button className="product__info-btn">Отложить в Избранное</button>
+                        <p className="product__info-price">{product.price} сом</p>
+                        <div className="product__info-prod">
+                            {
+                                user.carts?.some(el => el.id === product.id) ?
+                                    <button type="button" className="product__info-btn">ДОБАВЛЕНО
+                                        <span className="card__cart"><LiaOpencart size={22}/></span>
+                                    </button>
+                                    : <button type="button" className="cart__bottom-order" onClick={() => {
+                                        if ('id' in user){
+                                            addCarts(product)
+                                        } else{
+                                            navigate('/login')
+                                        }
+                                    }}>В КОРЗИНУ
+                                        <span className="card__cart"><LiaOpencart size={17}/></span>
+                                    </button>
+                            }
+                            <button className="cart__bottom-order product__info-fav" type='submit' onClick={() => favoritesHandler(product)}>
+                                {
+                                    favorites.some(el => el.id === product.id) ? <AiFillHeart size={20} color="red"/> : <AiOutlineHeart size={20}/>
+                                } В ИЗБРАННОЕ
+                            </button>
+                        </div>
                     </div>
                 </div>
 
