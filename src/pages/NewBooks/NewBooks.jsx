92fb0d8d5a27bd114d344264efbc3c10da7a0c15
karabaevagulmairam <karabaeva.gulmairam.pi9.7.20@gmail.com>
2023-09-16 16:50:54 +0600

    NewBooks.jsx

--- src/pages/NewBooks/NewBooks.jsx
+++ src/pages/NewBooks/NewBooks.jsx
@@ -1,36 +1,54 @@
-import React, {Fragment} from 'react';
+import React, {Fragment, useEffect, useState} from 'react';
 import Card from "../../components/Card/Card";
-import {Pagination} from "antd";
 import {useDispatch, useSelector} from "react-redux";
+import {getAllBooks} from "../../redux/reducers/books";
+import {Pagination} from "antd";
+import CardSkeleton from "../../components/CardSkeleton/CardSkeleton";
 
 const NewBooks = () => {
 
     const dispatch = useDispatch();
-
+    const [category] = useState('all');
     const {data, isLoading} = useSelector(store => store.books);
+    const [page, setPage] = useState(1);
+
+    useEffect(() => {
+        dispatch(getAllBooks())
+    }, []);
+
+
+    const showCount = data
+        .filter(item => category === "all" ? item : item.category === category)
+        .filter((item, idx) => idx + 1 <= page * 10 && idx >= page * 10 - 10).length;
+
+    const showCountLength = data
+        .filter(item => category === "all" ? item : item.category === category).length;
 
     return (
         <div className="newBooks">
             <div className="container">
+
                 <div className="catalog__row">
                     {
-                        data.filter((item) => item.year === '2023').filter((itm, idx) => idx <= 12).map((item, idx)=>(
-                            <Fragment key={item.id || idx}>
-                                <Card item={item}/>
-                            </Fragment>
-                        ))
+                        isLoading ? <CardSkeleton cards={12}/> :
+                            data.filter((item) => item.year === '2023').filter((itm, idx) => idx <= 12).map((item, idx)=>(
+                                <Fragment key={item.id || idx}>
+                                    <Card item={item}/>
+                                </Fragment>
+                            ))
                     }
                 </div>
-                {/*<p className="catalog__row-text">Показано {showCount} из {showCountLength} товаров</p>*/}
-                {/*<Pagination*/}
-                {/*    simple*/}
-                {/*    onChange={setPage}*/}
-                {/*    current={page}*/}
-                {/*    total={showCountLength}*/}
-                {/*    pageSize={10}*/}
-                {/*/>*/}
+                <p className="catalog__row-text">Показано {showCount} из {showCountLength} товаров</p>
+                <Pagination
+                    simple
+                    onChange={setPage}
+                    current={page}
+                    total={showCountLength}
+                    pageSize={10}
+                />
             </div>
-        </div>
+
+            </div>
 
     );
 };
